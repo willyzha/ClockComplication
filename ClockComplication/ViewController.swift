74f7fc14e64f37049714a3b2b8924ef5abe3ec08
WillyZhang <willyzha@gmail.com>
2017-06-04 14:14:46 -0400

    Initial Commit

---
+++ ClockComplication/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ClockComplication
+//
+//  Created by Willy Zhang on 2017-06-04.
+//  Copyright © 2017 Willy Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
